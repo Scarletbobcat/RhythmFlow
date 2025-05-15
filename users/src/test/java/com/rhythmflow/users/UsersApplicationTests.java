4a8e51902c4f0ea4039ca0d6a2491e4e747f382c
Tien Hoang <105090250+Scarletbobcat@users.noreply.github.com>
2025-05-15 14:49:50 +0200

    updated users test

--- users/src/test/java/com/rhythmflow/users/UsersApplicationTests.java
+++ users/src/test/java/com/rhythmflow/users/UsersApplicationTests.java
@@ -6,18 +6,9 @@ import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 class UsersApplicationTests {
-
-	private final ApplicationContext applicationContext;
-
-	public UsersApplicationTests(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
-
 	@Test
 	void contextLoads() {
-		assertNotNull(applicationContext, "The application context should not be null");
 	}
 
 }
